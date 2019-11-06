425652b0d3134741307fafe225fb87466e0f123d
Philippe <philippe2008@hotmail.com>
2019-11-05 23:23:30 -0500

    Update camera2d.

--- Assets/Scripts/Camera2D.cs
+++ Assets/Scripts/Camera2D.cs
@@ -5,21 +5,27 @@ public class Camera2D : MonoBehaviour {
 
 	private Vector2 velocity;
 
-	public float smoothTimeY;
-	public float smoothTimeX;
-	public float deltaY;
+	public float smoothTimeY = 0.2f;
+	public float smoothTimeX = 0.2f;
+	public float deltaY = 0.4f;
 	
 	public GameObject player;
 	
 	void Start(){
 		//player = GameObject.FindGameObjectWithTag("Player");
-		smoothTimeY = 0.2f;
-		smoothTimeX = 0.2f;
-		deltaY = 0.4f;
 	}
 	
-	void FixedUpdate(){
-		
+	void Update(){
+		if (player.transform.parent != null)
+			Movement();
+	}
+
+	void FixedUpdate() {
+		if (player.transform.parent == null)
+			Movement();
+	}
+
+	void Movement() {
 		float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
 		float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + deltaY, ref velocity.y, smoothTimeY);
 
